9eb8108d3ad8c04d6af93212869ed83904d29bff
Daphineatim <hopedaphine19@gmail.com>
2022-09-20 12:35:07 +0300

    Fix linters, Update style.js file

--- style.js
+++ style.js
@@ -6,55 +6,56 @@ const KEY = 'BOOKS_LIST';
 let BooksObject = [];
 
 class Books {
-    constructor() {
-        if (JSON.parse(localStorage.getItem(KEY)) != null) {
-            this.BooksObject = JSON.parse(localStorage.getItem(KEY));
-        } else {
-            this.BooksObject = [];
-        }
+  constructor() {
+    if (JSON.parse(localStorage.getItem(KEY)) != null) {
+      this.BooksObject = JSON.parse(localStorage.getItem(KEY));
+    } else {
+      this.BooksObject = [];
     }
-    add(book, author) {
-        this.BooksObject.push({
-            id: this.BooksObject.length,
-            title: book.value,
-            author: author.value,
-        });
-        localStorage.setItem('BOOKS_LIST', JSON.stringify(this.BooksObject));
-    }
-    remove(element) {
-        const { id } = element.dataset;
-        element.parentElement.remove();
-        this.BooksObject.splice(this.BooksObject.findIndex((item) => item.id === parseInt(id, 10)), 1);
-        localStorage.setItem('BOOKS_LIST', JSON.stringify(this.BooksObject));
-    }
+  }
+
+  add(book, author) {
+    this.BooksObject.push({
+      id: this.BooksObject.length,
+      title: book.value,
+      author: author.value,
+    });
+    localStorage.setItem('BOOKS_LIST', JSON.stringify(this.BooksObject));
+  }
+
+  remove(element) {
+    const { id } = element.dataset;
+    element.parentElement.remove();
+    this.BooksObject.splice(this.BooksObject.findIndex((item) => item.id === parseInt(id, 10)), 1);
+    localStorage.setItem('BOOKS_LIST', JSON.stringify(this.BooksObject));
+  }
 }
 function loadContent() {
-    booksList.innerHTML = '';
-    BooksObject.forEach((obj) => {
-        booksList.innerHTML += `<div class="book-container">
+  booksList.innerHTML = '';
+  BooksObject.forEach((obj) => {
+    booksList.innerHTML += `<div class="book-container">
                       <h4 class="text-1">${obj.title}</h4>
                       <h3 class="text-1">${obj.author}</h3>
                     <button type="button" onclick="removeBook(this)" class="btn" data-id="${obj.id}">Remove</button>
                     <hr>
                 </div>`;
-    });
+  });
 }
 function checkLocalStorage() {
-    if (JSON.parse(localStorage.getItem(KEY)) != null) {
-        BooksObject = JSON.parse(localStorage.getItem(KEY));
-        loadContent();
-    }
+  if (JSON.parse(localStorage.getItem(KEY)) != null) {
+    BooksObject = JSON.parse(localStorage.getItem(KEY));
+    loadContent();
+  }
 }
 const books = new Books();
 function addBook() {
-    books.add(book, author);
-    checkLocalStorage();
-    loadContent();
-    book.value = '';
-    author.value = '';
-    booksList.parentElement.classList.remove('hide');
-    author.parentElement.parentElement.classList.add('hide');
-    contactInfo.classList.add('hide');
+  books.add(book, author);
+  checkLocalStorage();
+  loadContent();
+  book.value = '';
+  author.value = '';
+  booksList.parentElement.classList.remove('hide');
+  author.parentElement.parentElement.classList.add('hide');
 }
 /* eslint-disable */
 function removeBook(element) {
@@ -62,9 +63,4 @@ function removeBook(element) {
 }
 /* eslint-enable */
 addBtn.addEventListener('click', addBook);
-list.addEventListener('click', () => {
-    booksList.parentElement.classList.remove('hide');
-    author.parentElement.parentElement.classList.add('hide');
-    contactInfo.classList.add('hide');
-});
 document.addEventListener('DOMContentLoaded', checkLocalStorage);
